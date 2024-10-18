efa5652c037dbe8fee8f356211442942559af9f0
ZaShkval <shkvalgrozny@gmail.com>
2024-10-18 21:57:08 +0500

    added file representation in admin

--- admin/routers/tickets.py
+++ admin/routers/tickets.py
@@ -2,10 +2,14 @@ from flask import Blueprint, render_template, session, request, url_for, redirec
 
 from admin import basic_get
 from admin.db.database import basic_get_all_asc, basic_create
-from admin.db.models import Ticket, Message
+from admin.db.models import Ticket, Message, Image
 from admin.db.models.messages import MessageSender
 from admin.service import generate_ticket_dict, generate_message_dict
 from admin.utils import auth_required
+
+import os
+import time
+from secrets import token_hex
 
 tickets_router = Blueprint(name='tickets_router', import_name='tickets_router')
 
@@ -45,7 +49,15 @@ def ticket_page_post(id: int):
     ticket = basic_get(Ticket, id=id)
     if not ticket:
         redirect(url_for('tickets_router.index'))
-    from pprint import pprint
-    pprint(request.form)
-    basic_create(Message, ticket_id=ticket.id, sender=MessageSender.ADMIN, content=request.form.get('msg'))
+    # processing file if exists
+    if request.files['file']:
+        file = request.files['file']
+        extension = file.filename.split('.')[-1]
+        # Warning! using /app/assets dir for file storage 
+        path = f'assets/{token_hex(8)}_{time.strftime("%Y%m%d%H%M")}.{extension}'
+        file.save(path)
+        image = basic_create(Image, path=path, filename=file.filename, extension=extension)
+        basic_create(Message, ticket_id=ticket.id, sender=MessageSender.ADMIN, content=request.form.get('msg'), image_id=image.id)
+    else:
+        basic_create(Message, ticket_id=ticket.id, sender=MessageSender.ADMIN, content=request.form.get('msg'))
     return redirect(url_for('tickets_router.ticket_page', id=ticket.id))
--- api.dockerfile
+++ api.dockerfile
@@ -6,3 +6,4 @@ RUN pip install --no-cache-dir -r /tmp/requirements.txt
 COPY . /app
 WORKDIR /app
 
+#RUN alembic upgrade head
